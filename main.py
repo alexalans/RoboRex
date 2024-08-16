f8e46c6d9ecc6eb5bd5aa36b91da0da2d1db3fb7
Alexa Lans <alexa@extralexa.nl>
2024-08-16 17:44:12 +0300

    Add light/dark mode detection, improve jumping accuracy (hi-score 1372)

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from pyautogui import ImageNotFoundException
 import PIL
 import time
 
-#TODO reverse jump logic when screen goes black then white
-
-JUMP_BREAK = 0.1
-JUMP_TIMING = 185
+JUMP_TIMING = 230
 
 robot_rex = True
 
@@ -16,6 +13,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
+
 try:
     driver.get("https://en.wikipedia.org/wiki/Main_Page")
 except WebDriverException:
@@ -31,18 +29,18 @@ else:
     start_y = dinosaur[1]
     pyautogui.press('space')
     time.sleep(1)
-    im = pyautogui.screenshot(imageFilename='my_screenshot.png', region=(start_x, start_y, 400, 100))
+    im = pyautogui.screenshot(imageFilename='my_screenshot.png ', region=(start_x, start_y, 400, 100))
 
 while robot_rex:
     image = pyautogui.screenshot(region=(start_x, start_y, 400, 100))
-    for cor in range((JUMP_TIMING - 40), (JUMP_TIMING + 30), 3):
+    mode = image.getpixel((1, 1))
+
+    for cor in range((JUMP_TIMING - 40), (JUMP_TIMING + 40), 3):
         pixel = image.getpixel((cor, 60))
-        if pixel[0] < 100:
+        if mode[0] > 100 > pixel[0] or mode[0] < 100 < pixel[0]:
             pyautogui.press('space')
-            time.sleep(JUMP_BREAK)
             break
         pixel = image.getpixel((cor, 40))
-        if pixel[0] < 100:
+        if mode[0] > 100 > pixel[0] or mode[0] < 100 < pixel[0]:
             pyautogui.press('space')
-            time.sleep(JUMP_BREAK)
             break
