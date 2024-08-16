0d3ba411f912009f96aa57e31216cf61c2c8fef1
Alexa Lans <alexa@extralexa.nl>
2024-08-16 16:07:36 +0300

    Add detection of middle bird, improve jumping accuracy (hi-score 730)

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import pyautogui
 import PIL
 import time
 
-JUMP_BREAK = 0.2
+#TODO middle flying bird
+#TODO first locate where the dino is on the screen before narrowing down the screenshot area and the pixel comparer
+#TODO reverse jump logic when screen goes black then white
+
+JUMP_BREAK = 0.1
 JUMP_TIMING = 160
 
 chrome_options = webdriver.ChromeOptions()
@@ -20,14 +24,18 @@ pyautogui.press('space')
 time.sleep(1)
 
 robot_rex = True
-#TODO first locate where the dino is on the screen before narrowing down the screenshot area and the pixel comparer
 
 im = pyautogui.screenshot(imageFilename='my_screenshot.png', region=(320,320, 400, 100))
 
 while robot_rex:
     image = pyautogui.screenshot(region=(320,320, 400, 100))
-    for cor in range((JUMP_TIMING - 40), (JUMP_TIMING + 30), 2):
+    for cor in range((JUMP_TIMING - 40), (JUMP_TIMING + 30), 3):
         pixel = image.getpixel((cor, 60))
+        if pixel[0] < 100:
+            pyautogui.press('space')
+            time.sleep(JUMP_BREAK)
+            break
+        pixel = image.getpixel((cor, 40))
         if pixel[0] < 100:
             pyautogui.press('space')
             time.sleep(JUMP_BREAK)
