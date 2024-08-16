d5f0317e3dbdf0c3f6b6564bd148cbf6f4f7a013
Alexa Lans <alexa@extralexa.nl>
2024-08-16 16:34:01 +0300

    Locate dinosaur on screen before running script instead of hard-coding coordinates

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 import pyautogui
+from pyautogui import ImageNotFoundException
 import PIL
 import time
 
-#TODO middle flying bird
-#TODO first locate where the dino is on the screen before narrowing down the screenshot area and the pixel comparer
 #TODO reverse jump logic when screen goes black then white
 
 JUMP_BREAK = 0.1
-JUMP_TIMING = 160
+JUMP_TIMING = 185
+
+robot_rex = True
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -20,15 +21,20 @@ try:
 except WebDriverException:
     time.sleep(2)
 
-pyautogui.press('space')
-time.sleep(1)
-
-robot_rex = True
-
-im = pyautogui.screenshot(imageFilename='my_screenshot.png', region=(320,320, 400, 100))
+try:
+    dinosaur = pyautogui.locateOnScreen('dinosaur.png')
+except ImageNotFoundException:
+    print("Dinosaur game not found. Make sure your internet connection is turned off")
+    robot_rex = False
+else:
+    start_x = dinosaur[0]
+    start_y = dinosaur[1]
+    pyautogui.press('space')
+    time.sleep(1)
+    im = pyautogui.screenshot(imageFilename='my_screenshot.png', region=(start_x, start_y, 400, 100))
 
 while robot_rex:
-    image = pyautogui.screenshot(region=(320,320, 400, 100))
+    image = pyautogui.screenshot(region=(start_x, start_y, 400, 100))
     for cor in range((JUMP_TIMING - 40), (JUMP_TIMING + 30), 3):
         pixel = image.getpixel((cor, 60))
         if pixel[0] < 100:
